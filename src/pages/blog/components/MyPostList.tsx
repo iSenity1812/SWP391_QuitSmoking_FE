0e4bdb09f0590a3257a469396b05059300aa0530
DuyLTA <letrananhduy2005@gmail.com>
2025-06-25 08:55:08 +0700

    Fix

--- src/pages/blog/components/MyPostList.tsx
+++ src/pages/blog/components/MyPostList.tsx
@@ -76,7 +76,6 @@ const MyPostsList: React.FC<MyPostsListProps> = ({
                     </Button>
                     <div>
                         <h2 className="text-2xl font-bold text-slate-900 dark:text-white">Bài viết của tôi</h2>
-                        <p className="text-slate-600 dark:text-slate-300">{posts.length} bài viết đã xuất bản</p>
                     </div>
                 </div>
             </div>
