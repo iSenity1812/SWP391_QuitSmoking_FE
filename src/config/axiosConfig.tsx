49a822567b592f5a9c85fa247b0153bf49ccc330
Gia Bảo <trunglontq1@gmail.com>
2025-07-11 17:38:24 +0700

    Fix notification logic, optimize timeout, async email/notification

--- src/config/axiosConfig.tsx
+++ src/config/axiosConfig.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosConfig = axios.create({
   baseURL: 'http://localhost:8080', // Đã bỏ /api ở đây để đúng root path
-  timeout: 10000, // 10 seconds timeout
+  timeout: 20000, // 20 seconds timeout
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
